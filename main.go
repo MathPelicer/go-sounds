3cfa38410ed378a33eada94a796a0aa13799769a
MathPelicer <matheuspelicer58@gmail.com>
2025-05-19 22:20:10 -0300

    feat: add utils package and start laying songs

--- main.go
+++ main.go
@@ -1,25 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io"
-	"io/fs"
-	"log"
-	"math/rand"
 	"os"
-	"path"
-	"runtime"
 	"strings"
-	"time"
+
+	"go-sounds/utils"
 
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-	"github.com/faiface/beep"
-	"github.com/faiface/beep/effects"
-	"github.com/faiface/beep/mp3"
-	"github.com/faiface/beep/speaker"
 )
 
 type model struct {
@@ -28,33 +19,7 @@ type model struct {
 	cursor   int
 	selected map[int]struct{}
 	quitting bool
-}
-
-type Song struct {
-	streamer beep.StreamSeekCloser
-	format   beep.Format
-	name     string
-}
-
-type SongsList struct {
-	songs []Song
-}
-
-type Control string
-
-const (
-	START   Control = "start"
-	PLAYING Control = "playing"
-	NEXT    Control = "n"
-	PAUSE   Control = "p"
-)
-
-func verifyOS() string {
-	if runtime.GOOS == "windows" {
-		return "C:/Users/mathe/Music/System Of A Down/Hypnotize"
-	} else {
-		return "/home/mathe/Music"
-	}
+	playlist utils.SongsList
 }
 
 var (
@@ -94,20 +59,21 @@ type item string
 func (i item) FilterValue() string { return "" }
 
 func initialModel() model {
-	dir := verifyOS()
-	songs := listSongs(dir)
-	playlist := &SongsList{}
-	playlist.addAllSongsToPlaylist(songs, dir)
+	dir := utils.VerifyOS()
+	songs := utils.ListSongs(dir)
+	playlist := &utils.SongsList{}
+	playlist.AddAllSongsToPlaylist(songs, dir)
 
 	items := []list.Item{}
-	for _, song := range playlist.songs {
-		items = append(items, item(song.name))
+	for _, song := range playlist.Songs {
+		items = append(items, item(song.Name))
 	}
 
 	l := list.New(items, itemDelegate{}, 20, 20)
 	return model{
 		list:     l,
 		selected: make(map[int]struct{}),
+		playlist: *playlist,
 	}
 }
 
@@ -130,6 +96,8 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				m.list.CursorUp()
 			}
 		case " ", "enter":
+			control := utils.START
+			utils.StartSong(control, &m.playlist, m.list.Index())
 			_, ok := m.selected[m.cursor]
 			if ok {
 				delete(m.selected, m.cursor)
@@ -155,73 +123,6 @@ func main() {
 		fmt.Printf("Error %v", err)
 		os.Exit(1)
 	}
-}
-
-func (q *SongsList) Add(songs ...Song) {
-	q.songs = append(q.songs, songs...)
-}
-
-func (q *SongsList) addAllSongsToPlaylist(songs []string, dir string) {
-	for songIndex := range songs {
-		f, err := os.Open(path.Join(dir, songs[songIndex]))
-		if err != nil {
-			log.Fatal("cant open file")
-		}
-
-		streamer, format, err := mp3.Decode(f)
-		if err != nil {
-			log.Fatal("cant decode file")
-		}
-
-		song := Song{
-			streamer: streamer,
-			format:   format,
-			name:     songs[songIndex],
-		}
-
-		q.Add(song)
-	}
-}
-
-func listSongs(dir string) []string {
-	root := os.DirFS(dir)
-
-	songFiles, err := fs.Glob(root, "*.mp3")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var songs []string
-	songs = append(songs, songFiles...)
-
-	return songs
-}
-
-func printSongs(songs []string) {
-	for index, song := range songs {
-		fmt.Printf("%d: %s\n", index, song)
-	}
-}
-
-func selectSong(songs []string) int {
-	var songIndex int
-	for {
-		fmt.Println("Select a song number: ")
-		_, err := fmt.Scanf("%d", &songIndex)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if songIndex <= len(songs) && songIndex > 0 {
-			break
-		}
-
-		fmt.Println("Thats not a valid song")
-	}
-
-	return songIndex
 }
 
 //func main() {
@@ -290,78 +191,3 @@ func selectSong(songs []string) int {
 //		}
 //	}
 //}
-
-func startSong(control Control, playlist *SongsList, playlistIndex int) *beep.Ctrl {
-	if control == START || control == NEXT {
-		sr := playlist.songs[playlistIndex].format
-		speaker.Init(sr.SampleRate, sr.SampleRate.N(time.Second/10))
-	}
-
-	ctrl := &beep.Ctrl{Streamer: playlist.songs[playlistIndex].streamer, Paused: false}
-	volume := &effects.Volume{
-		Streamer: ctrl,
-		Base:     2,
-		Volume:   -4.0,
-		Silent:   false,
-	}
-
-	speaker.Play(beep.Seq(volume))
-
-	return ctrl
-}
-
-func goToNextSong(playlist *SongsList, playlistIndex *int) {
-	speaker.Lock()
-	playlist.songs[*playlistIndex].streamer.Close()
-	*playlistIndex += 1
-	speaker.Unlock()
-}
-
-func goToSong(playlist *SongsList, playlistIndex *int, songIndex int) {
-	speaker.Lock()
-	playlist.songs[*playlistIndex].streamer.Close()
-	*playlistIndex = songIndex
-	speaker.Unlock()
-}
-
-func goToRandomSong(playlist *SongsList, playlistIndex *int) {
-	speaker.Lock()
-	playlist.songs[*playlistIndex].streamer.Close()
-	*playlistIndex = rand.Intn(len(playlist.songs))
-	speaker.Unlock()
-}
-
-func waitForUserInput(c chan string) {
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		char, _ := reader.ReadString('\n')
-
-		c <- strings.TrimSuffix(char, "\n")
-	}
-}
-
-func isSongFinished(playlist *SongsList, playlistIndex int) bool {
-	songLen := playlist.songs[playlistIndex].streamer.Len()
-	songPos := playlist.songs[playlistIndex].streamer.Position()
-	return songLen == songPos
-}
-
-func songProgress(playlist *SongsList, playlistIndex int) {
-	songLen := playlist.songs[playlistIndex].streamer.Len()
-	chunkSize := songLen / 30
-
-	chunksListened := playlist.songs[playlistIndex].streamer.Position() / chunkSize
-
-	progressString := "\r" + playlist.songs[playlistIndex].name + " ["
-
-	for i := 0; i < 30; i++ {
-		if i <= chunksListened {
-			progressString += "#"
-		} else {
-			progressString += " "
-		}
-	}
-	progressString += "]"
-
-	fmt.Printf("\r%s", progressString)
-}
---
+++ utils/utils.go
@@ -0,0 +1,188 @@
+package utils
+
+import (
+	"bufio"
+	"fmt"
+	"io/fs"
+	"log"
+	"math/rand"
+	"os"
+	"path"
+	"runtime"
+	"strings"
+	"time"
+
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/effects"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+)
+
+type Song struct {
+	streamer beep.StreamSeekCloser
+	format   beep.Format
+	Name     string
+}
+
+type SongsList struct {
+	Songs []Song
+}
+
+type Control string
+
+const (
+	START   Control = "start"
+	PLAYING Control = "playing"
+	NEXT    Control = "n"
+	PAUSE   Control = "p"
+)
+
+func VerifyOS() string {
+	if runtime.GOOS == "windows" {
+		return "C:/Users/mathe/Music/System Of A Down/Hypnotize"
+	} else {
+		return "/home/mathe/Music"
+	}
+}
+
+func (q *SongsList) Add(songs ...Song) {
+	q.Songs = append(q.Songs, songs...)
+}
+
+func (q *SongsList) AddAllSongsToPlaylist(songs []string, dir string) {
+	for songIndex := range songs {
+		f, err := os.Open(path.Join(dir, songs[songIndex]))
+		if err != nil {
+			log.Fatal("cant open file")
+		}
+
+		streamer, format, err := mp3.Decode(f)
+		if err != nil {
+			log.Fatal("cant decode file")
+		}
+
+		song := Song{
+			streamer: streamer,
+			format:   format,
+			Name:     songs[songIndex],
+		}
+
+		q.Add(song)
+	}
+}
+
+func ListSongs(dir string) []string {
+	root := os.DirFS(dir)
+
+	songFiles, err := fs.Glob(root, "*.mp3")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var songs []string
+	songs = append(songs, songFiles...)
+
+	return songs
+}
+
+func PrintSongs(songs []string) {
+	for index, song := range songs {
+		fmt.Printf("%d: %s\n", index, song)
+	}
+}
+
+func SelectSong(songs []string) int {
+	var songIndex int
+	for {
+		fmt.Println("Select a song number: ")
+		_, err := fmt.Scanf("%d", &songIndex)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if songIndex <= len(songs) && songIndex > 0 {
+			break
+		}
+
+		fmt.Println("Thats not a valid song")
+	}
+
+	return songIndex
+}
+
+func StartSong(control Control, playlist *SongsList, playlistIndex int) *beep.Ctrl {
+	if control == START || control == NEXT {
+		sr := playlist.Songs[playlistIndex].format
+		speaker.Init(sr.SampleRate, sr.SampleRate.N(time.Second/10))
+	}
+
+	ctrl := &beep.Ctrl{Streamer: playlist.Songs[playlistIndex].streamer, Paused: false}
+	volume := &effects.Volume{
+		Streamer: ctrl,
+		Base:     2,
+		Volume:   -4.0,
+		Silent:   false,
+	}
+
+	speaker.Play(beep.Seq(volume))
+
+	return ctrl
+}
+
+func goToNextSong(playlist *SongsList, playlistIndex *int) {
+	speaker.Lock()
+	playlist.Songs[*playlistIndex].streamer.Close()
+	*playlistIndex += 1
+	speaker.Unlock()
+}
+
+func goToSong(playlist *SongsList, playlistIndex *int, songIndex int) {
+	speaker.Lock()
+	playlist.Songs[*playlistIndex].streamer.Close()
+	*playlistIndex = songIndex
+	speaker.Unlock()
+}
+
+func goToRandomSong(playlist *SongsList, playlistIndex *int) {
+	speaker.Lock()
+	playlist.Songs[*playlistIndex].streamer.Close()
+	*playlistIndex = rand.Intn(len(playlist.Songs))
+	speaker.Unlock()
+}
+
+func waitForUserInput(c chan string) {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		char, _ := reader.ReadString('\n')
+
+		c <- strings.TrimSuffix(char, "\n")
+	}
+}
+
+func isSongFinished(playlist *SongsList, playlistIndex int) bool {
+	songLen := playlist.Songs[playlistIndex].streamer.Len()
+	songPos := playlist.Songs[playlistIndex].streamer.Position()
+	return songLen == songPos
+}
+
+func songProgress(playlist *SongsList, playlistIndex int) {
+	songLen := playlist.Songs[playlistIndex].streamer.Len()
+	chunkSize := songLen / 30
+
+	chunksListened := playlist.Songs[playlistIndex].streamer.Position() / chunkSize
+
+	progressString := "\r" + playlist.Songs[playlistIndex].Name + " ["
+
+	for i := 0; i < 30; i++ {
+		if i <= chunksListened {
+			progressString += "#"
+		} else {
+			progressString += " "
+		}
+	}
+	progressString += "]"
+
+	fmt.Printf("\r%s", progressString)
+}
